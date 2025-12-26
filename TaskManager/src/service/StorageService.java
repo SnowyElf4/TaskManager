50df7d2235912f6838fcd3708dfe0453c0b36d46
SnowyElf <ahdeza.188@gmail.com>
2025-12-26 16:01:01 +0500

    Exception handler

--- TaskManager/src/service/StorageService.java
+++ TaskManager/src/service/StorageService.java
@@ -1,55 +1,78 @@
 package src.service;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import src.task.Task;
 import src.user.User;
 
-import java.io.FileReader;
-import java.io.Writer;
-import java.io.FileWriter;
-import java.io.Reader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import com.google.gson.reflect.TypeToken;
 
 public class StorageService {
     private Gson gson = new Gson();
-    private final String USERS_FILE = "data/user.json";
-    private final String TASKS_FILE = "data/task.json";
-    private List<User> users = new ArrayList<User>();
-    private List<Task> tasks = new ArrayList<Task>();
+
+    private final String USERS_FILE = "TaskManager/data/users.json";
+    private final String TASKS_FILE = "TaskManager/data/tasks.json";
+
+    private List<User> users = new ArrayList<>();
+    private List<Task> tasks = new ArrayList<>();
+
+    public StorageService() {
+        File dataDir = new File("data");
+        if (!dataDir.exists())
+            dataDir.mkdir();
+    }
 
     public void loadUsers() {
-        try (Reader reader = new FileReader(USERS_FILE)) {
+        File file = new File(USERS_FILE);
+        if (!file.exists()) {
+            users = new ArrayList<>();
+            return;
+        }
+
+        try (Reader reader = new FileReader(file)) {
             users = gson.fromJson(reader, new TypeToken<List<User>>() {
             }.getType());
-        } catch (Exception e) {
-            System.out.println(e);
+            if (users == null)
+                users = new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+            users = new ArrayList<>();
         }
     }
 
     public void loadTasks() {
-        try (Reader reader = new FileReader(TASKS_FILE)) {
+        File file = new File(TASKS_FILE);
+        if (!file.exists()) {
+            tasks = new ArrayList<>();
+            return;
+        }
+
+        try (Reader reader = new FileReader(file)) {
             tasks = gson.fromJson(reader, new TypeToken<List<Task>>() {
             }.getType());
-        } catch (Exception e) {
-            System.out.println(e);
+            if (tasks == null)
+                tasks = new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+            tasks = new ArrayList<>();
         }
     }
 
     public void saveUsers() {
         try (Writer writer = new FileWriter(USERS_FILE)) {
             gson.toJson(users, writer);
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void saveTasks() {
         try (Writer writer = new FileWriter(TASKS_FILE)) {
             gson.toJson(tasks, writer);
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -60,5 +83,4 @@ public class StorageService {
     public List<Task> getTasks() {
         return tasks;
     }
-
 }
